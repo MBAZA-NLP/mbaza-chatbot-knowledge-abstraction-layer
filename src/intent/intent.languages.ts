97ac3ab47cd03d6de159711334abf728e5c75900
Samuel Rutunda <samuel@digitalumuganda.com>
2022-05-30 14:31:20 +0200

    first commit of the KBAL to the github repository

---
+++ src/intent/intent.languages.ts
@@ -0,0 +1,21 @@
+export enum IntentLanguages {
+    ENGLISH = "en",
+    FRENCH = "fr",
+    KINYARWANDA = "rw"
+};
+
+export type IntentTranslationMapping = {
+  [key in IntentLanguages as string]: string;
+}
+
+export const ALLOWED_LANGUAGES = Object.keys(IntentLanguages).map((key) => IntentLanguages[key]);
+
+export const DEFAULT_LOCALE = IntentLanguages.ENGLISH;
+export const DEFAULT_RESPONSE: IntentTranslationMapping = {
+  "en": "I'm sorry, I can't help you with that!",
+  "fr": "Je suis désolé, je ne peux pas t'aider avec ça !",
+  "rw": "Mumbabarire, sinshobora kugufasha kubyo!",
+};  
+
+
+export const UNSUPPORTED_LANGUAGE_MESSAGE = "Unsupported language!";
